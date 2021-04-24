3e5a850345766249bf6f09728365031b5eeac7ea
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-04-24 20:28:03 +0200

    Add Keymou

---
+++ Casks/keymou.rb
@@ -0,0 +1,10 @@
+cask "keymou" do
+  version "1.2.9"
+  sha256 "784a74b42e08b22e262f4501e8dc0be8ac0e0b1c09d9cde39f7085814562a89c"
+
+  url "https://manytricks.com/download/_do_not_hotlink_/keymou129.dmg"
+  name "Keymou"
+  homepage "https://manytricks.com/keymou/"
+
+  app "Keymou.app"
+end

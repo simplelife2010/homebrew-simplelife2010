5f89a279bb8365dc8291653d88e8461028e8ff7c
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-05-02 19:43:14 +0200

    Add iCMD

---
+++ Casks/icmd.rb
@@ -0,0 +1,10 @@
+cask "icmd" do
+  version "1.8.8"
+  sha256 "3054688fb6c128ca81d956f5b622f766a1e72215f5a6f96150d1d11a65ab0068"
+
+  url "https://github.com/simplelife2010/icmd/releases/download/v1.8.8/iCMD.app.zip"
+  name "icmd"
+  homepage "https://www.icmd.app/"
+
+  app "iCMD.app"
+end

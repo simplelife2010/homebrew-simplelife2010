a3dfe3cb0ad96790b3659a8119a4228cd96f679e
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-05-02 19:47:40 +0200

    Fix URL

--- Casks/icmd.rb
+++ Casks/icmd.rb
@@ -2,7 +2,7 @@ cask "icmd" do
   version "1.8.8"
   sha256 "3054688fb6c128ca81d956f5b622f766a1e72215f5a6f96150d1d11a65ab0068"
 
-  url "https://github.com/simplelife2010/icmd/releases/download/v1.8.8/iCMD.app.zip"
+  url "https://storage.googleapis.com/icmd.app/releases/latest.zip?product_id=XWqFFvro6E3QE8tq1nFuEQ%3D%3D&product_permalink=ndbNRk&sale_id=8uWhQdK5jgbfpia2PkazGA%3D%3D"
   name "icmd"
   homepage "https://www.icmd.app/"
 

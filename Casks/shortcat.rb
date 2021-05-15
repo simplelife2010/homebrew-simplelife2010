bf28f9c77e54de2dfba00e126b2a8d3d7185c31e
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-05-15 15:59:25 +0200

    Add Shortcat

---
+++ Casks/shortcat.rb
@@ -0,0 +1,10 @@
+cask "shortcat" do
+  version "0.7.11"
+  sha256 "bb62fcb6c644c1ccca356d0ac1ede1b29ebbc1cebe5809e186b3ba295ed51b07"
+
+  url "https://files.shortcatapp.com/v0.7.11/Shortcat.zip"
+  name "Shortcat"
+  homepage "https://shortcatapp.com/"
+
+  app "Shortcat.app"
+end

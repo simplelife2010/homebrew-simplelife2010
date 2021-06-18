cc34aa4c6b882168de5f134b99709e69d60e855d
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-06-18 18:24:31 +0200

    Downgrade Capture

--- Casks/logitech-capture.rb
+++ Casks/logitech-capture.rb
@@ -14,7 +14,7 @@ cask "logitech-capture" do
   }
 
   uninstall script: {
-    executable: "/Applications/Utilities/LogiCapture Uninstaller.app",
+    executable: "/Applications/Utilities/LogiCapture Uninstaller.app/Contents/Macos/LogiCapture Uninstaller",
     sudo:       true,
   }
 end

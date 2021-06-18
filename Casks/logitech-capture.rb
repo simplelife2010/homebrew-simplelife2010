55329255934b8686ca610dfabf1dc8a430203a2b
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-06-18 15:45:38 +0200

    Add Logitech Capture

---
+++ Casks/logitech-capture.rb
@@ -0,0 +1,10 @@
+cask "logitech-capture" do
+  version "2.06.34"
+
+  url "https://download01.logi.com/web/ftp/pub/techsupport/capture/Capture_2.06.34.zip"
+  name "Logitech Capture"
+  desc "Logitech Capture Video Recording & Streaming Software"
+  homepage "https://www.logitech.com/en-us/product/capture"
+
+  app "Logi Capture.app"
+end

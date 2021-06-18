9bf467f477f531f104230c71c0246afd63e0b93b
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-06-18 18:00:03 +0200

    Downgrade Capture

--- Casks/logitech-capture.rb
+++ Casks/logitech-capture.rb
@@ -1,15 +1,15 @@
 cask "logitech-capture" do
-  version "2.06.34"
+  version "2.06.08"
   sha256 :no_check
 
-  url "https://download01.logi.com/web/ftp/pub/techsupport/capture/Capture_2.06.34.zip",
+  url "https://download01.logi.com/web/ftp/pub/techsupport/capture/Capture_2.06.08.zip",
       verified: "download01.logi.com"
   name "Logitech Capture"
   desc "Video Recording & Streaming Software"
   homepage "https://www.logitech.com/en-us/product/capture"
 
   installer script: {
-    executable: "Logi Capture Installer 2.06.34.app/Contents/MacOS/LogiMgr Installer",
+    executable: "Logi Capture Installer 2.06.08.app/Contents/MacOS/LogiMgr Installer",
     sudo:       true,
   }
 

b599070cd604efd633b9d3164541433a4ac83a1e
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-06-19 20:46:38 +0200

    Uninstall Capture

--- Casks/logitech-capture.rb
+++ Casks/logitech-capture.rb
@@ -1,15 +1,20 @@
 cask "logitech-capture" do
-  version "2.02.253"
+  version "2.06.34"
   sha256 :no_check
 
-  url "https://download01.logi.com/web/ftp/pub/techsupport/capture/Capture_2.02.253.zip",
+  url "https://download01.logi.com/web/ftp/pub/techsupport/capture/Capture_2.06.34.zip",
       verified: "download01.logi.com"
   name "Logitech Capture"
   desc "Video Recording & Streaming Software"
   homepage "https://www.logitech.com/en-us/product/capture"
 
   installer script: {
-    executable: "Logi Capture Installer 2.02.253.app/Contents/MacOS/LogiMgr Installer",
+    executable: "Logi Capture Installer 2.06.34.app/Contents/MacOS/LogiMgr Installer",
+    sudo:       true,
+  }
+
+  uninstall script: {
+    executable: "/Applications/Utilities/LogiCapture Uninstaller.app/Contents/MacOS/LogiCapture Uninstaller",
     sudo:       true,
   }
 end

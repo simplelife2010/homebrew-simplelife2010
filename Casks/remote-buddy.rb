92f0bb6e45cead64e36b6cb69dceaee7e4591803
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-08-03 21:06:51 +0200

    Remote Buddy

---
+++ Casks/remote-buddy.rb
@@ -0,0 +1,10 @@
+cask "remote-buddy" do
+  version "2.0.21"
+  sha256 :no_check
+
+  url "https://www.iospirit.com/products/remotebuddy/download/RemoteBuddy.zip"
+  name "Remote Buddy"
+  homepage "https://www.iospirit.com/products/remotebuddy/"
+
+  app "Remote Buddy.app"
+end

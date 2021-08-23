d65dde001a99a7c5ad6fc9769d3ee54a8555562a
Bruno Vetter <bruno.vetter@deutschebahn.com>
2021-08-23 17:00:23 +0200

    Add Poly Lens

---
+++ Casks/poly-lens.rb
@@ -0,0 +1,10 @@
+cask "poly-lens" do
+  version "1.1.1"
+  sha256 :no_check
+
+  url "https://swupdate.lens.poly.com/lens-desktop-mac/1.1.1/1.1.1/PolyLens-1.1.1.dmg"
+  name "Poly Lens"
+  homepage "https://www.poly.com/de/de/products/services/cloud/poly-lens/app#"
+
+  app "Poly Lens.app"
+end
